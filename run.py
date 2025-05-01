6cde63f51cacb50ca201ba64039d78a80d19f792
ITEAA <hanta67@naver.com>
2025-05-01 20:07:50 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app, db
 from flask_migrate import Migrate
 
-migrate = Migrate(app, db)
-
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run()
