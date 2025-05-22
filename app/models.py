70eb1db3dba44b2608cb7e654831826294df6cd3
ITEAA <hanta67@naver.com>
2025-05-22 20:04:04 +0900

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,16 +4,18 @@ class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     plate_number = db.Column(db.String(20), unique=True, nullable=False)
     car_type = db.Column(db.String(20))
+    records = db.relationship('ParkingRecord', backref='car', lazy=True)
 
 class ParkingSpot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     spot_number = db.Column(db.String(10), unique=True, nullable=False)
     is_occupied = db.Column(db.Boolean, default=False)
+    records = db.relationship('ParkingRecord', backref='spot', lazy=True)
 
 class ParkingRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    car_id = db.Column(db.Integer, db.ForeignKey('car.id'))
-    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'))
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'), nullable=False)
     entry_time = db.Column(db.DateTime)
     exit_time = db.Column(db.DateTime)
     fee = db.Column(db.Float)
