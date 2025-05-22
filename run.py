7073d6725e99749779e3f2c6bd8c5654b771e98e
ITEAA <hanta67@naver.com>
2025-05-22 20:23:14 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import db, create_app
 
 app = create_app()
+
+# ✅ 아래 코드 임시 추가 (처음 1회만 필요)
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
