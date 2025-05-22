c76224d674ade33d5a9b050e1c7f14884656bb1a
ITEAA <hanta67@naver.com>
2025-05-22 20:03:54 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from .routes import register_routes
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parking.db'
-    db.init_app(app)
-    migrate.init_app(app, db)
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+    # DB 연결 설정 (SQLAlchemy URI 사용)
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    register_routes(app)
 
     return app
